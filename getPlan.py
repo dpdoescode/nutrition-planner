b641e6542456ada67b42f98f95e44fc233b23713
Malachi Davey <malachilddavey@gmail.com>
2026-06-23 19:28:30 +0000

    validate meal option

--- getPlan.py
+++ getPlan.py
@@ -8,4 +8,12 @@ userid = 'cli_user_1'
 
 url = 'https://api.edamam.com/api/meal-planner/v1/{mealplan_appid}/select'
 # response = requests.post(url, )
+response = requests.get(url)
 
+meals = ['breakfast', 'lunch', 'dinner', 'snack']
+
+meal = input("Which meals do you eat per day?(breakfast, lunch, dinner, snack): ")
+
+
+
+#print(response.json())
