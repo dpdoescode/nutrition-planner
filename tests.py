df29cb1742a6ff044c2bc9a7d70be8cf7eab64a9
Malachi Davey <malachilddavey@gmail.com>
2026-06-25 19:20:41 +0000

    meals, email, weight, calories unittests added

--- tests.py
+++ tests.py
@@ -4,4 +4,21 @@ from validators import validateDays, validateMeals, validateEmail, validateWeigh
 class TestInputs(unittest.TestCase):
     def test_validateMeals(self):
         self.assertEqual(validateMeals("breakfast, lunch"), (True, ["breakfast", "lunch"]))
+
+
+    def test_validateEmail(self):
+        self.assertTrue(validateEmail("malachi@fsu.edu"))
+        self.assertFalse(validateEmail("notanemail"))
+        self.assertFalse("missing@domain")
+    
+    def test_validateWeight(self):
+        self.assertTrue(validateWeight("150"))
+        self.assertFalse(validateWeight("49"))
+        self.assertFalse(validateWeight("501"))
+
+    def test_validateCalories(self):
+        self.assertTrue(validateCalories("2000"))
+        self.assertFalse(validateCalories("999"))
+        self.assertFalse(validateCalories("3501"))
+    
         
